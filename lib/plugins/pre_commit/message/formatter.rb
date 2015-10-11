fd79ef1bc372ba9ae78aa0b0ea6db1e25d606ac2
Cristian Oliveira da Rosa <contato@cristianoliveira.com.br>
2015-10-10 23:52:14 -0300

    Adding Ruby Docs

--- lib/plugins/pre_commit/message/formatter.rb
+++ lib/plugins/pre_commit/message/formatter.rb
@@ -1,9 +1,11 @@
 module PreCommit
   module Message
+    ##
+    # Responsible for formating the given error message output
+    # keeping only the message errors generated for checkstyle.java
     class Formatter
       def format(output)
-        p output
-        return nil unless output
+        return nil if output.nil? || output.empty?
         output.gsub(/(   *)|^\w+(.*)[.|\n]/,'')
       end
     end
