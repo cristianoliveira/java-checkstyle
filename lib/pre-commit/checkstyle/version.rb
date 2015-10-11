7ee166ae2560a14dada8d46b6abda1b53a12b421
Cristian Oliveira da Rosa <contato@cristianoliveira.com.br>
2015-10-11 14:39:11 -0300

    Version: 0.0.2

---
+++ lib/pre-commit/checkstyle/version.rb
@@ -0,0 +1,16 @@
+##
+# This a fork version of the original
+# checkstyle:
+# https://github.com/secondrotation/pre-commit-checkstyle
+#
+module PreCommit
+  ##
+  # CheckStyle Version
+  # Authors:
+  #  Allen Madsen (Original Version 0.0.1) 
+  #  Cristian Oliveira
+  #  Alex Rocha
+  module Checkstyle
+    VERSION = '0.0.2'
+  end
+end
